b5d8b80c7dc0f86b9337faedee132c0420c60896
Andrey work <example@gmail.com>
2023-04-25 18:11:10 +0300

    Ready main page and base logic

--- js/script.js
+++ js/script.js
@@ -1,31 +1,87 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Получаем все элементы с классом .radio-button
-    const radioButtons = document.querySelectorAll('.radio-button');
+'use strict';
 
-    // Добавляем обработчик события на каждый элемент
-    radioButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            // Получаем связанный input элемент
-            const input = button.previousElementSibling;
-            
-            // Удаляем класс active со всех radio-button
-            radioButtons.forEach(btn => {
-                btn.classList.remove('radio-button__active');
-                btn.parentElement.classList.remove('card__active');
+document.addEventListener('DOMContentLoaded', () => {
+    // Наши вопросы
+    const question = document.querySelector('.question');
+    const questions = [
+        'Для кого вы ищете учебное заведение?',
+        'В каком городе планируете поступать?',
+        'Какое образование уже есть?',
+        'Куда планируете поступать?',
+        'Какую форму обучения предпочитаете?',
+        'Рассматриваете платное обучение?',
+        'Какая специальность интересует?',
+        'Как скоро планируете поступать?',
+        'Ваша подборка готова! 🥳 Куда нам отправить её?'
+    ];
+
+
+    // Пробегаем по всем карточкам
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach(item => {
+        item.addEventListener('click', () => {
+            const input = item.querySelector('input');
+            const span = item.querySelector('span');
+
+            cards.forEach(btn => {
+                btn.classList.remove('card__active');
+                btn.querySelector('span').classList.remove('radio-button__active');
             });
-            
-            // Добавляем класс active только к текущему элементу
-            button.classList.add('radio-button__active');
-            button.parentElement.classList.remove('card__active');
-            
-            // Устанавливаем свойство checked для выбранного input элемента
+
+            span.classList.add('radio-button__active');
+            item.classList.add('card__active');
+
             input.checked = true;
         });
     });
 
+    const button = document.querySelectorAll('button');
+    let stepCount = 1;
+    
+    button.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const buttonMore = document.querySelector('button.more');
+            const buttonBack = document.querySelector('button.back');
+            const wrapperBlock = document.querySelector('.wrapper-block__content');
+            const step = document.querySelector('.step');
+            
+            // Если нажали на "Далее", то счетчик шагов увеличивается на 1, при нажатии "Назад", уменьшается на 1
+            if(stepCount < questions.length && stepCount > 0){
+                if(btn.classList.contains('more')){
+                    stepCount++;
+                } else {
+                    stepCount--;
+                } 
+            } else {
+                
+            }
+            console.log(stepCount < questions.length);
+            
+
+            // Показываем текущий блок шага
+            for(let key of wrapperBlock.children){
+                const wrapperContent = key;
+                if((key.classList[0] == `wrapper-block__step-${stepCount}`)){
+                    wrapperContent.classList.remove('display-none');
+                } else {
+                    wrapperContent.classList.add('display-none');
+                }
+            }
+
+            // Добавляем кнопку "Назад", если шаг больше 1, и убираем, если шаг меньше
+            (stepCount > 1) ? buttonBack.classList.remove('opacity') : buttonBack.classList.add('opacity');
+            
+            // Меняем цифру в шаге
+            step.innerHTML = `Шаг ${stepCount}/9`; 
+
+            question.innerHTML = questions[stepCount-1];
 
 
-
+    
+        });
+    });
+    
 
 
 
