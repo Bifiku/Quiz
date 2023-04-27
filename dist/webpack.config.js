db658bdbe71dc8a39444be3968433e27cc06ca90
Bifiku <120630628+Bifiku@users.noreply.github.com>
2023-04-27 20:09:24 +0300

    test dist

---
+++ dist/webpack.config.js
@@ -0,0 +1,1 @@
+const path=require("path"),CopyWebpackPlugin=require("copy-webpack-plugin");module.exports={entry:"../src/js/script.js",output:{filename:"bundle.js",path:path.resolve(__dirname,"..","dist")},mode:"production",plugins:[new CopyWebpackPlugin({patterns:[{from:"*",to:"."},{from:"*/*",to:"."}]})]};
